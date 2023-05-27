69a16c164809f0c1a14bd3ef1018e0a8aa081295
LegitBen <lepend1n8691@gmail.com>
2023-05-27 14:16:52 +0300

    Initial dfs

---
+++ dfs.py
@@ -0,0 +1,25 @@
+def dfs(graph, start):
+    visited = set()  # Множество посещенных вершин
+    stack = [(start, 0)]  # Стек для обхода, включая длину шага
+
+    while stack:
+        vertex, distance = stack.pop()  # Извлекаем вершину и длину шага из стека
+
+        if vertex not in visited:
+            visited.add(vertex)  # Посещаем вершину
+            print(vertex)
+
+            # Добавляем непосещенные соседние вершины с учетом расстояния в стек
+            neighbors = graph[vertex]
+            for neighbor, step in neighbors:
+                if neighbor not in visited:
+                    stack.append((neighbor, distance + step))
+
+graph = {
+    1: [(3, 2)],
+    2: [(4, 1)],
+    3: [],
+    4: [(2, 3)]
+}
+
+dfs(graph, 1)
